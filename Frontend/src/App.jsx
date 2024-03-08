aee7cd3c19e499059bb58c3cada561cd45bca046
Arup3105 <devrup3105@gmail.com>
2024-03-08 23:34:03 +0530

    frontend changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Login from './Components/Login';
 import Register from './Components/Register';
 import './Components/register.css';
 import Feed from './Components/Feed';
-import Company from './Components/Company';
 import Admin from './Components/Admin';
 import SeeCompany from './Components/SeeCompany';
 import PostsByCompany from './Components/PostByCompany'
@@ -19,7 +18,6 @@ const App = () => {
         <Route path="feed" element={<Feed />} />
         <Route path="seeCompany/:startYear/:endYear" element={<SeeCompany />} />
         <Route path="/postsByCompany/:companyName/:startYear/:endYear" element={<PostsByCompany />} />
-        <Route path="company" element={<Company />} />
         <Route path="admin" element={<Admin />} />
       </Routes>
     </div>
